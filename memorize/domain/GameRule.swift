b37e99408cae591035dd0db2925e82734a71111b
Artem Japparov <japparov@surfstudio.ru>
2021-01-02 19:52:51 +0300

    промежуточный этап имплементации правил игры

---
+++ memorize/domain/GameRule.swift
@@ -0,0 +1,22 @@
+//
+//  GameRule.swift
+//  memorize
+//
+//  Created by Artem Japparov on 02.01.2021.
+//
+
+import Foundation
+
+class GameRule {
+    
+    let requiredCardsCount: Int
+    
+    init(requiredCardsCount: Int) {
+        self.requiredCardsCount = requiredCardsCount
+    }
+    
+// первоначально пробовал    func chooseCard(card: MemoryGame<Comparable>.Card) {
+    func chooseCard(card: MemoryGame.Card) {
+    
+    }
+}
