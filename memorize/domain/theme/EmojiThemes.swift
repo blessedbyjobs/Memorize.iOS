4281677c36ab0fbab79f4168255222cc557fcf01
Artem Japparov <japparov@surfstudio.ru>
2021-01-06 14:30:52 +0300

    минорный рефакторинг

--- memorize/domain/theme/EmojiThemes.swift
+++ memorize/domain/theme/EmojiThemes.swift
@@ -8,48 +8,49 @@
 import Foundation
 
 struct EmojiThemes {
-
+    
     // MARK: - Properties
-       
-       private var theme: String = "FRUITS"
-       private var themeLabels: Array<String>
-       
-       var currentTheme: String {
-           return getTheme(theme: theme).nonNull
-       }
-       
-       init() {
-           themeLabels = Array(themes.keys)
-       }
-
-       // MARK: - Public methods
-       
-       func getTheme() -> String {
-           return themes[theme]!
-       }
-       
-       mutating func getTheme(at position: Int) -> String {
-           self.theme = themeLabels[position % themeLabels.count]
-           return self.getTheme()
-       }
-       
-       mutating func getRandTheme() -> String {
-           let position = themeLabels.count.random
-           return getTheme(at: position)
-       }
-       
-       func getTheme(theme tempTheme: String) -> String? {
-           return themes[tempTheme]
-       }
-
-       // MARK: - Private methods
-       
-       private var themes: Dictionary<String, String> = [
-           "FRUITS":  "🍏🍎🍐🍊🍋🍌🍉🍇🍑🍒🍈🍓🍍🥝",
-           "FACES": "😀😄😁😆😂😊☺️😉🙂",
-           "ANIMALS": "🐶🐱🐼🐻🦊🐰🐮🐷🐵🐤🦆🦉🐴",
-           "WEATHER": "⚡️🌪🌈☀️⛅️☁️🌦🌧🌩❄️💨☔️🌊",
-           "SPORTS": "⚽️🏀🏈🎾🏐🏓🏒⛸🥊🥋",
-           "COMPUTERS": "💻🖥⌨️🖱🕹💽💾📼📟📠📱"
-       ]
+    
+    var currentTheme: String {
+        return getTheme(theme: theme)
+    }
+    
+    // MARK: - Private properties
+    
+    private var theme: String = "FRUITS"
+    private var themeLabels: Array<String>
+    
+    
+    private var themes: Dictionary<String, String> = [
+        "FRUITS":  "🍏🍎🍐🍊🍋🍌🍉🍇🍑🍒🍈🍓🍍🥝",
+        "FACES": "😀😄😁😆😂😊☺️😉🙂",
+        "ANIMALS": "🐶🐱🐼🐻🦊🐰🐮🐷🐵🐤🦆🦉🐴",
+        "WEATHER": "⚡️🌪🌈☀️⛅️☁️🌦🌧🌩❄️💨☔️🌊",
+        "SPORTS": "⚽️🏀🏈🎾🏐🏓🏒⛸🥊🥋",
+        "COMPUTERS": "💻🖥⌨️🖱🕹💽💾📼📟📠📱"
+    ]
+    
+    init() {
+        themeLabels = Array(themes.keys)
+    }
+    
+    // MARK: - Public methods
+    
+    func getTheme() -> String {
+        return themes[theme].nonNull
+    }
+    
+    func getTheme(theme tempTheme: String) -> String {
+        return themes[tempTheme].nonNull
+    }
+    
+    mutating func getTheme(at position: Int) -> String {
+        self.theme = themeLabels[position % themeLabels.count]
+        return currentTheme
+    }
+    
+    mutating func getRandTheme() -> String {
+        let position = themeLabels.count.random
+        return getTheme(at: position)
+    }
 }
